acbcdf34ea13354736c3d11099fc362ac7e2b3bc
BaurinAnton <baurinanton2013@yandex.ru>
2022-09-06 09:22:30 +0300

    fixed breakpoints on borders

--- config/breakpoints.ts
+++ config/breakpoints.ts
@@ -4,16 +4,16 @@ type TypeBreakpoints = {
     phone: number
     tablet: number
     laptop: number
-    widescreen: number
+    desktop: number
   }
   
-  const { phone, tablet, laptop, widescreen } = stls
+  const { phone, tablet, laptop, desktop } = stls
   
   const breakpoints: TypeBreakpoints = {
     phone: +phone.slice(0, -2),
     tablet: +tablet.slice(0, -2),
     laptop: +laptop.slice(0, -2),
-    widescreen: +widescreen.slice(0, -2),
+    desktop: +desktop.slice(0, -2),
   }
   
   export default breakpoints
