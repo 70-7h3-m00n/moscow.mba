a9896989b9bacb76ba8b48ef8e410e96338b774b
BaurinAnton <baurinanton2013@yandex.ru>
2022-09-09 12:20:02 +0300

    component CardAuthor has been developed

---
+++ components/cards/CardAuthor.tsx
@@ -0,0 +1,59 @@
+import Image from 'next/image'
+import cn from 'classnames'
+
+import { TypeLibJournalArticleCardAuthor } from '@/types/index'
+
+import { ImgJournalArticle } from '@/components/images'
+
+import stls from "@/styles/components/cards/CardAuthor.module.sass"
+
+type TypeCardQuoteProps = {
+    quote: TypeLibJournalArticleCardAuthor
+    textAlign?: 'start' | 'end'
+    fontWeightAuthorName?: 400 | 700
+}
+
+const CardAuthor = ({
+    quote,
+    textAlign,
+    fontWeightAuthorName
+}: TypeCardQuoteProps) => {
+    return (
+        <div className={cn(
+            stls.cardAuthor,
+            textAlign === 'start'
+                ? stls.textAlignStart
+                : textAlign === 'end'
+                    ? stls.textAlignEnd
+                    : '')
+        }>
+            <div className={stls.image}>
+                <Image
+                    src={quote?.portrait.url || undefined}
+                    width={quote?.portrait.width && 48}
+                    height={quote?.portrait.height && 48}
+                    alt={quote?.portrait.alt || 'Фото'}
+                    className={stls.img} />
+            </div>
+            <div className={stls.content}>
+                {
+                    quote?.label
+                        ? <p className={stls.label}>{quote?.label}</p>
+                        : quote?.authorPosition
+                            ? <p className={stls.position}>{quote?.authorPosition}</p>
+                            : 'Автор'
+                }
+                <p className={cn(
+                    stls.authorName,
+                    fontWeightAuthorName === 400
+                        ? stls.fontWeightNormal
+                        : fontWeightAuthorName === 700
+                            ? stls.fontWeightBold
+                            : ''
+                )}>{quote?.authorName}</p>
+            </div>
+        </div >
+    )
+}
+
+export default CardAuthor
--- components/cards/index.ts
+++ components/cards/index.ts
@@ -2,3 +2,4 @@
 export { default as CardContact } from '@/components/cards/CardContact'
 export { default as CardJournalArticle } from '@/components/cards/CardJournalArticle'
 export { default as CardProgram } from '@/components/cards/CardProgram'
+export { default as CardAuthor } from '@/components/cards/CardAuthor'
