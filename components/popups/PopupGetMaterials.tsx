c8cb939aa6ad68fda9d58a7bd973997f339376ad
BaurinAnton <baurinanton2013@yandex.ru>
2022-09-23 15:46:26 +0300

    implemented form submission with PopupGetMaterials statuses

--- components/popups/PopupGetMaterials.tsx
+++ components/popups/PopupGetMaterials.tsx
@@ -6,6 +6,7 @@ import { getClassNames } from '@/helpers/index'
 
 import { TypeClassNames } from '@/types/index'
 import { FormAlpha, FormJournalArticle } from '@/components/forms'
+import { IconLoader, IconClose } from '@/components/icons'
 
 import stls from '@/styles/components/popups/PopupGetMaterials.module.sass'
 
@@ -25,6 +26,8 @@ const PopupGetMaterials = ({
 }: TypeSectionPopupCoursesOnTopicProps) => {
     const [open, setOpen] = useState(false)
     const [openLoader, setOpenLoader] = useState(false)
+    console.log(open)
+    console.log("OpenLoader", openLoader)
     return (
         <>
             <div
@@ -32,13 +35,17 @@ const PopupGetMaterials = ({
                 onClick={handlePopupGetMaterials}
             ></div>
             <div className={cn([stls.container], getClassNames({ classNames })) || undefined}>
-                <div className={stls.pdfPopupGetMaterials}>
-                    <Image
-                        src={pdfPopupGetMaterials}
-                        width={190}
-                        height={230}
-                    />
-                </div>
+                {
+                    (!openLoader)
+                        ? <div className={stls.pdfPopupGetMaterials}>
+                            <Image
+                                src={pdfPopupGetMaterials}
+                                width={190}
+                                height={230}
+                            />
+                        </div>
+                        : ''
+                }
                 <div className={stls.topLinePopupGetMaterials}>
                     <Image
                         src={linePopupGetMaterials}
@@ -56,18 +63,36 @@ const PopupGetMaterials = ({
                 <button
                     className={stls.closed}
                     onClick={handlePopupGetMaterials}></button>
-                <div className={stls.title}>
-                    <p className={stls.text}>{'7 ошибок разрушения карьеры.\nГайд по профессиям IT '}</p>
+                <div className={openLoader || open ? stls.formContent : ''}>
+                    <div className={stls.title}>
+                        <p className={stls.text}>{'7 ошибок разрушения карьеры.\nГайд по профессиям IT '}</p>
+                    </div>
+                    <div className={stls.form}>
+                        <FormJournalArticle
+                            programTitle={null}
+                            setOpenLoader={setOpenLoader}
+                            setOpen={setOpen}
+                            classNames={[stls.submitButton]}>
+                            <span className={stls.submitText}>{'получить бесплатно'}</span>
+                        </FormJournalArticle>
+                    </div>
                 </div>
-                <div className={stls.form}>
-                    <FormJournalArticle
-                        programTitle={null}
-                        setOpenLoader={setOpenLoader}
-                        setOpen={setOpen}
-                        classNames={[stls.submitButton]}>
-                        <span className={stls.submitText}>{'получить бесплатно'}</span>
-                    </FormJournalArticle>
-                </div>
+                {
+                    (open && !openLoader)
+                        ? <div className={stls.formComplited}>
+                            <p className={stls.formComplitedTitle}>Спасибо! Файл отправлен Вам на почту.</p>
+                            <button
+                                className={stls.formComplitedClosed}
+                                onClick={() => setOpen(open => !open)}>Вернуться назад
+                            </button>
+                        </div>
+                        : ''
+                }
+                {
+                    (!open && openLoader)
+                        ? <div className={stls.formLoading}><IconLoader /></div>
+                        : ''
+                }
             </div>
         </>
     )
