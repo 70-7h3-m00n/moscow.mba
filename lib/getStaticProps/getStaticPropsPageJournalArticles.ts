5d8dcb586dbf091056f907940302ac910423731b
novailoveyou <novailoveyoux@gmail.com>
2022-12-14 02:02:05 +0300

    removed createBlended from data fetching

--- lib/getStaticProps/getStaticPropsPageJournalArticles.ts
+++ lib/getStaticProps/getStaticPropsPageJournalArticles.ts
@@ -1,33 +1,24 @@
 import { GetStaticPropsContext } from 'next'
-import {
-  TypePageJournalArticlesProps,
-} from '@/types/index'
-import { gql } from '@apollo/client'
-import apolloClient from '@/lib/apolloClient'
-import { revalidate } from '@/config/index'
-import { createBlended } from '@/helpers/index'
-import { routesBack } from 'config'
+import { TypePageJournalArticlesProps } from '@/types/index'
+import axios from 'axios'
+import { revalidate, routesBack } from '@/config/index'
 
 const getStaticPropsPageJournalArticles = async ({
-  context
+	context
 }: {
-  context: GetStaticPropsContext
+	context: GetStaticPropsContext
 }): Promise<{
-  props: TypePageJournalArticlesProps
-  revalidate: number | boolean
+	props: TypePageJournalArticlesProps
+	revalidate: number | boolean
 }> => {
-  const gspContextParamsJournalArticle =
-    context?.params?.journalArticle?.toString() || null
-  const urlJournal = `${routesBack.root}${routesBack.journal}`
-  const res = await fetch(urlJournal)
-  const data = await res.json()
-  return {
-    props: {
-      ...data,
-      programs: createBlended(data?.programs),
-    },
-    revalidate: revalidate.default
-  }
+	const res = await axios.get(
+		`${routesBack.root}${routesBack.getStaticPropsPageJournalArticles}`
+	)
+
+	return {
+		props: res.data,
+		revalidate: revalidate.default
+	}
 }
 
 export default getStaticPropsPageJournalArticles
