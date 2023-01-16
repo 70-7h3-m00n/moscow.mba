0bd4c6de94603b4c994186407a8407e0feb5c495
Evgenie <nestroykozni@gmail.com>
2023-01-16 17:24:35 +0300

    fix(ValidationErros): delete noindex tag and add CSR for StickyBottomContainer

--- components/layout/StickyBottomContainer.tsx
+++ components/layout/StickyBottomContainer.tsx
@@ -1,5 +1,5 @@
 import stls from '@/styles/components/layout/StickyBottomContainer.module.sass'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
 import { Overlay, StickyBottom } from '@/components/layout'
 import { AskQuestion } from '@/components/general'
@@ -12,10 +12,11 @@ const StickyBottomContainer = () => {
   const [clickedAsk, setClickedAsk] = useState(false)
   const [isStickyBottomShown, setIsStickyBottomShown] = useState(false)
   const [stickyHasBeenClosed, setStickyHasBeenClosed] = useState(false)
+  const [isLoaded, setIsLoaded] = useState(false)
 
   const router = useRouter()
   const at = useAt()
-  
+
   const containerClasses = [
     stls.container,
     isStickyBottomShown && !stickyHasBeenClosed
@@ -30,31 +31,33 @@ const StickyBottomContainer = () => {
   }
 
   const handleAskQuestionFormClose = () => setClickedAsk(false)
-  if (at.journal) return null
+
+  useEffect(() => setIsLoaded(true), [])
+
+  if (!isLoaded || at.journal) return null
+
   return (
-    <noindex>
-      <div className={containerClasses.join(' ')}>
-        {clickedAsk ? (
-          <>
-            <Overlay handleAskQuestionFormClose={handleAskQuestionFormClose} />
-            <AskQuestionForm
-              handleAskQuestionFormClose={handleAskQuestionFormClose}
-            />
-          </>
-        ) : (
-          <AskQuestion
-            handleClickedAskQuestion={handleClickedAskQuestion}
-            stickyShown={isStickyBottomShown}
+    <div className={containerClasses.join(' ')}>
+      {clickedAsk ? (
+        <>
+          <Overlay handleAskQuestionFormClose={handleAskQuestionFormClose} />
+          <AskQuestionForm
+            handleAskQuestionFormClose={handleAskQuestionFormClose}
           />
-        )}
-        <StickyBottom
-          openStickyModule={() => setIsStickyBottomShown(true)}
-          hideStickyModule={() => setIsStickyBottomShown(false)}
-          closeStickyModule={() => setStickyHasBeenClosed(true)}
-          clickedAsk={clickedAsk}
+        </>
+      ) : (
+        <AskQuestion
+          handleClickedAskQuestion={handleClickedAskQuestion}
+          stickyShown={isStickyBottomShown}
         />
-      </div>
-    </noindex>
+      )}
+      <StickyBottom
+        openStickyModule={() => setIsStickyBottomShown(true)}
+        hideStickyModule={() => setIsStickyBottomShown(false)}
+        closeStickyModule={() => setStickyHasBeenClosed(true)}
+        clickedAsk={clickedAsk}
+      />
+    </div>
   )
 }
 
