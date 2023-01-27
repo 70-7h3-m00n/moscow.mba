a95bf86630b31db857783014e8ac4caf73ad7e5b
Evgenie <nestroykozni@gmail.com>
2023-01-27 14:04:01 +0300

    feat(Reviews): improve ImgTemplate props

--- components/images/ImgTemplate.tsx
+++ components/images/ImgTemplate.tsx
@@ -1,90 +1,64 @@
 import stls from '@/styles/components/images/ImgTemplate.module.sass'
 import { TypeClassNames, TypeImg } from '@/types/index'
 import Image from 'next/image'
-import { StaticImageData } from 'next/image'
 import cn from 'classnames'
 import { base64pixel } from '@/config/index'
 import { getClassNames } from '@/helpers/index'
+import { ComponentPropsWithRef } from 'react'
 
-type TypeImgTemplateProps = TypeClassNames &
-  TypeImg & {
-    readonly src: StaticImageData | string
-    readonly alt: string
-    readonly title?: string
-    readonly faded?: boolean
-    readonly filter?: boolean
-    readonly filterAlt?: boolean
-    readonly darken?: boolean
-    readonly priority?: boolean
-    readonly layout?: 'intrinsic' | 'fixed' | 'responsive' | 'fill'
-    readonly objectFit?:
-      | 'contain'
-      | 'cover'
-      | 'fill'
-      | 'none'
-      | 'scale-down'
-      | 'inherit'
-      | 'initial'
-      | 'revert'
-      | 'unset'
-    readonly objectPosition?: any // replace any type with right one
-  }
+type TypeImgTemplateProps = TypeClassNames & {
+	readonly faded?: boolean
+	readonly filter?: boolean
+	readonly filterAlt?: boolean
+	readonly darken?: boolean
+} & ComponentPropsWithRef<typeof Image>
 
 const ImgTemplate = ({
-  classNames,
-  width,
-  height,
-  src,
-  alt,
-  title,
-  faded,
-  filter,
-  filterAlt,
-  darken,
-  priority,
-  layout,
-  objectFit,
-  objectPosition
+	src,
+	title,
+	alt,
+	classNames,
+	faded,
+	filter,
+	filterAlt,
+	darken,
+	...imageProps
 }: TypeImgTemplateProps) => {
-  return (
-    <span
-      className={
-        cn(
-          [stls.container],
-          { [stls.containerFaded]: faded },
-          { [stls.containerFilter]: filter },
-          { [stls.containerFilterAlt]: filterAlt },
-          { [stls.containerDarken]: darken },
-          getClassNames({ classNames })
-        ) || undefined
-      }
-      title={title}>
-      <span
-        className={cn({
-          [stls.faded]: faded,
-          [stls.filter]: filter,
-          [stls.filterAlt]: filterAlt,
-          [stls.darken]: darken,
-          [stls.isHidden]: !faded && !filter && !filterAlt && !darken
-        })}></span>
-      {src && (
-        <Image
-          src={src}
-          alt={alt}
-          width={width}
-          height={height}
-          className={stls.img}
-          placeholder='blur'
-          blurDataURL={base64pixel}
-          priority={priority}
-          layout={layout}
-          objectFit={objectFit}
-          objectPosition={objectPosition}
-          aria-label={title || alt}
-        />
-      )}
-    </span>
-  )
+	return (
+		<span
+			className={
+				cn(
+					[stls.container],
+					{ [stls.containerFaded]: faded },
+					{ [stls.containerFilter]: filter },
+					{ [stls.containerFilterAlt]: filterAlt },
+					{ [stls.containerDarken]: darken },
+					getClassNames({ classNames })
+				) || undefined
+			}
+			title={title}>
+			<span
+				className={cn({
+					[stls.faded]: faded,
+					[stls.filter]: filter,
+					[stls.filterAlt]: filterAlt,
+					[stls.darken]: darken,
+					[stls.isHidden]: !faded && !filter && !filterAlt && !darken
+				})}></span>
+			{src && (
+				<Image
+					src={src}
+					alt={alt}
+					title={title}
+					className={stls.img}
+					placeholder='blur'
+					blurDataURL={base64pixel}
+					aria-label={title || alt}
+					{...imageProps}
+				/>
+			)}
+		</span>
+	)
 }
 
 export default ImgTemplate
