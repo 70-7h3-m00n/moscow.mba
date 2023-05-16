388984f554f941d48c8b4e784c0da9975c0d8b51
Dmitry Borisenko <brdima92@gmail.com>
2023-05-16 10:47:53 +0300

    fix: disallow non-existent pages

--- pages/programs/course/online/[slug].tsx
+++ pages/programs/course/online/[slug].tsx
@@ -6,29 +6,38 @@ import { CourseOnlineProgram } from '@/components/pages'
 import { SeoOrganizationJsonLd, SeoPagesProgram } from '@/components/seo'
 
 const PageProgramsCourseOnlineProgram = ({ program, programs }) => {
-  usePageHandleContext({ programs, program })
+	usePageHandleContext({ programs, program })
 
-  return (
-    <>
-      <SeoPagesProgram program={program} />
-      <CourseOnlineProgram program={program} teachers={program?.teachers} />
-    </>
-  )
+	return (
+		<>
+			<SeoPagesProgram program={program} />
+			<CourseOnlineProgram program={program} teachers={program?.teachers} />
+		</>
+	)
 }
 
-export const getStaticProps: GetStaticProps = async context =>
-  await handleGetStaticProps({
-    page: routesFront.program,
-    context,
-    type: 'course',
-    format: 'online'
-  })
+export const getStaticProps: GetStaticProps = async context => {
+	const data = await handleGetStaticProps({
+		page: routesFront.program,
+		context,
+		type: 'course',
+		format: 'online'
+	})
+
+	if (!data.props?.programs?.find(prog => prog.slug === context.params.slug)) {
+		return {
+			notFound: true
+		}
+	} else {
+		return data
+	}
+}
 
 export const getStaticPaths: GetStaticPaths = async () =>
-  await handleGetStaticPaths({
-    page: routesFront.program,
-    type: 'course',
-    format: 'online'
-  })
+	await handleGetStaticPaths({
+		page: routesFront.program,
+		type: 'course',
+		format: 'online'
+	})
 
 export default PageProgramsCourseOnlineProgram
--- pages/programs/mini/online/[slug].tsx
+++ pages/programs/mini/online/[slug].tsx
@@ -6,29 +6,38 @@ import { OnlineProgram } from '@/components/pages'
 import { SeoOrganizationJsonLd, SeoPagesProgram } from '@/components/seo'
 
 const PageProgramsMiniOnlineProgram = ({ program, programs }) => {
-  usePageHandleContext({ programs, program })
+	usePageHandleContext({ programs, program })
 
-  return (
-    <>
-      <SeoPagesProgram program={program} />
-      <OnlineProgram program={program} teachers={program?.teachers} />
-    </>
-  )
+	return (
+		<>
+			<SeoPagesProgram program={program} />
+			<OnlineProgram program={program} teachers={program?.teachers} />
+		</>
+	)
 }
 
-export const getStaticProps: GetStaticProps = async context =>
-  await handleGetStaticProps({
-    page: routesFront.program,
-    context,
-    type: 'mini',
-    format: 'online'
-  })
+export const getStaticProps: GetStaticProps = async context => {
+	const data = await handleGetStaticProps({
+		page: routesFront.program,
+		context,
+		type: 'mini',
+		format: 'online'
+	})
+
+	if (!data.props?.programs?.find(prog => prog.slug === context.params.slug)) {
+		return {
+			notFound: true
+		}
+	} else {
+		return data
+	}
+}
 
 export const getStaticPaths: GetStaticPaths = async () =>
-  await handleGetStaticPaths({
-    page: routesFront.program,
-    type: 'mini',
-    format: 'online'
-  })
+	await handleGetStaticPaths({
+		page: routesFront.program,
+		type: 'mini',
+		format: 'online'
+	})
 
 export default PageProgramsMiniOnlineProgram
