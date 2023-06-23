327aaa88912d0a29d5aa7c9c5c50a6579051a1de
Dmitry Borisenko <brdima92@gmail.com>
2023-06-23 16:10:06 +0300

    fix/ make random number of placesLeft

--- components/costs/PlacesLeft.tsx
+++ components/costs/PlacesLeft.tsx
@@ -1,23 +1,32 @@
-const PlacesLeft = () => {
+import { useEffect, useState } from 'react'
+
+const PlacesLeft = ({ uniqueKey }) => {
 	const currentDate = new Date()
 	const currentDay = currentDate.getDate()
 
-	let placesLeft =
-		(1 <= currentDay && currentDay <= 3) ||
-		(11 <= currentDay && currentDay <= 13) ||
-		(21 <= currentDay && currentDay <= 23)
-			? 7
-			: (4 <= currentDay && currentDay <= 6) ||
-			  (14 <= currentDay && currentDay <= 16) ||
-			  (24 <= currentDay && currentDay <= 26)
-			? 5
-			: (7 <= currentDay && currentDay <= 10) ||
-			  (17 <= currentDay && currentDay <= 20) ||
-			  (27 <= currentDay && currentDay <= 31)
-			? 2
-			: 10
+	const [places, setPlaces] = useState(4)
+	const divisible = typeof uniqueKey === 'number' ? uniqueKey % 10 : 8
 
-	return <>{placesLeft}</>
+	useEffect(() => {
+		const placesLeft =
+			(1 <= currentDay && currentDay <= 3) ||
+			(11 <= currentDay && currentDay <= 13) ||
+			(21 <= currentDay && currentDay <= 23)
+				? Math.floor((divisible + 15) / 2)
+				: (4 <= currentDay && currentDay <= 6) ||
+				  (14 <= currentDay && currentDay <= 16) ||
+				  (24 <= currentDay && currentDay <= 26)
+				? Math.floor((divisible + 10) / 3)
+				: (7 <= currentDay && currentDay <= 10) ||
+				  (17 <= currentDay && currentDay <= 20) ||
+				  (27 <= currentDay && currentDay <= 31)
+				? Math.floor((divisible + 5) / 3)
+				: 10
+
+		setPlaces(placesLeft)
+	}, [places])
+
+	return <>{places}</>
 }
 
 export default PlacesLeft
