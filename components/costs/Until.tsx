dc617e6cb742668732b295c78382164feeffe883
Dmitry Borisenko <brdima92@gmail.com>
2023-07-24 16:36:47 +0300

    fix/ change UntilDay

--- components/costs/Until.tsx
+++ components/costs/Until.tsx
@@ -2,14 +2,41 @@ import { useRouter } from 'next/router'
 
 // need to take Until date from Strapi, if there's no date count it
 
+/*
+По зачислениям:
+Зачисления:
+27.07 и 28.07
+
+Август
+10.08  и 11.08
+21.08 и 22.08
+30.08 и 31.08
+
+Сентябрь
+11.09 и 12.09
+20.09 и 21.09  10
+28.09 и 29.09  20 - 31
+
+Октябрь
+10.10 и 11.10
+19.10 и 20.10
+30.10 и 31.10
+
+На 27.07 сейчас самое первое дело перенести, т.к. это будет мотиватором для КЛ оставить больше заявок на этой неделе, а нам возможность быстрее их закрыть.
+Парные дни - т.к. один день зачисляем, на второй идет дозачисление.
+Остальные дни были из расчета выходных дней.
+*/
 const setLastDayOfMonth = (currentDate: Date) => {
 	currentDate.setMonth(currentDate.getMonth() + 1, 0)
 }
 
 const setNextDay = (currentDate: Date, currentDay: number) =>
-	currentDay <= 22
-		? currentDate.setDate(currentDay <= 11 ? 11 : 22) // changed 10 to 11
-		: setLastDayOfMonth(currentDate)
+	currentDay <= 20
+		? currentDate.setDate(currentDay <= 10 ? 10 : 20)
+		: currentDay === 28
+		? currentDate.setDate(28)
+		: currentDate.setDate(27)
+// : setLastDayOfMonth(currentDate)
 
 const Until = ({ preposition = true, executive = false }) => {
 	const { locale } = useRouter()
