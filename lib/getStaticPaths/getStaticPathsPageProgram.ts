6729f7181073e427a797c3e092435c68f6998649
Dmitry Borisenko <brdima92@gmail.com>
2023-12-07 13:42:36 +0300

    feat/ change getStaticPaths for program page to apolo client

--- lib/getStaticPaths/getStaticPathsPageProgram.ts
+++ lib/getStaticPaths/getStaticPathsPageProgram.ts
@@ -1,55 +1,57 @@
 import { TypePageProgramPaths, TypePageProgramPathsQuery } from '@/types/index'
 import axios from 'axios'
 import { fallback, routesBack } from '@/config/index'
+import { ApolloClient, gql } from '@apollo/client'
+import client from '../apolloClient'
 
 type TypeGetStaticPathsPageProgram = {
-  format?: string
-  type?: string
+	format?: string
+	type?: string
 }
 
 const getStaticPathsPageProgram = async ({
-  format,
-  type = 'mini'
+	format,
+	type = 'mini'
 }: TypeGetStaticPathsPageProgram): Promise<{
-  paths: TypePageProgramPaths
-  fallback: boolean | 'blocking'
+	paths: TypePageProgramPaths
+	fallback: boolean | 'blocking'
 }> => {
-  const res = await axios.get(
-    `${routesBack.root}${routesBack.getStaticPathsPageProgram}/${type}`
-  )
+	// const res = await axios.get(
+	//   `${routesBack.root}${routesBack.getStaticPathsPageProgram}/${type}`
+	// )
 
-  const paths = res.data.paths
+	// const paths = res.data.paths
 
-  return {
-    paths,
-    fallback: fallback.default
-  }
+	// return {
+	//   paths,
+	//   fallback: fallback.default
+	// }
 
-  // const res = await apolloClient.query<TypePageProgramPathsQuery>({
-  //   query: gql`
-  //     query GetStaticPathsPageProgram($type: String!) {
-  //       programs: products(where: { category: { type: $type } }) {
-  //         slug
-  //       }
-  //     }
-  //   `,
-  //   variables: {
-  //     type
-  //   }
-  // })
+	const res = await client.query<TypePageProgramPathsQuery>({
+		query: gql`
+			query GetStaticPathsPageProgram($type: String!) {
+				programs: products(where: { category: { type: $type } }) {
+					slug
+				}
+			}
+		`,
+		variables: {
+			type
+		}
+	})
 
-  // return {
-  //   paths: Array.from(
-  //     new Set(
-  //       res.data?.programs?.map(program => ({
-  //         params: {
-  //           slug: program?.slug || 'program'
-  //         }
-  //       }))
-  //     )
-  //   ) || [{ params: { slug: 'program' } }],
-  //   fallback: fallback.default
-  // }
+	return {
+		paths: Array.from(
+			new Set(
+				res.data?.programs?.map(program => ({
+					params: {
+						slug: program?.slug || 'program'
+					}
+				}))
+			)
+		) || [{ params: { slug: 'program' } }],
+		fallback: fallback.default
+	}
 }
 
 export default getStaticPathsPageProgram
--- modules/program-page/widgets/Reviews/components/Rating/Rating.tsx
+++ modules/program-page/widgets/Reviews/components/Rating/Rating.tsx
@@ -32,8 +32,15 @@ export const Rating = ({ className }: RatingProps) => {
 	return (
 		<ul className={cn(className, stls.content)}>
 			<li className={cn(stls.large, stls.left)}>
-				<h3>Получите конкурентное преимущество на своем карьерном пути</h3>
-				<p>test</p>
+				<Image
+					src={'/assets/images/program/reviews-rating-photo-2.jpg'}
+					alt='Студентка'
+					width={90}
+					height={90}
+				/>
+				<p className={stls.left__title}>
+					Получите конкурентное преимущество на своем карьерном пути
+				</p>
 			</li>
 			<li className={stls.large}>
 				<Image
