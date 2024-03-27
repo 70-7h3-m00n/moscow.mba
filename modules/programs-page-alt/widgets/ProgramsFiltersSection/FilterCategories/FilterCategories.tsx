7e9151fb443979a10b5fdd8d63679bf038c50b44
Dmitry Borisenko <brdima92@gmail.com>
2024-03-27 11:43:41 +0300

    test

--- modules/programs-page-alt/widgets/ProgramsFiltersSection/FilterCategories/FilterCategories.tsx
+++ modules/programs-page-alt/widgets/ProgramsFiltersSection/FilterCategories/FilterCategories.tsx
@@ -12,16 +12,17 @@ import {
 import { InfoTooltip } from '@/components/popups/InfoTooltip/InfoTooltip'
 import routesFront from '@/config/routesFront'
 import { useRouter } from 'next/navigation'
+import Link from 'next/link'
 
 export const FilterCategories = ({ className }: FilterCategoriesProps) => {
 	const router = useRouter()
 	const { state, dispatch } = useContext(ProgramsPageContext)
 
 	const handlerOnClick = (value: FilterTypeProgramEnum) => {
-		if (value === FilterTypeProgramEnum.executive) {
-			router.push(routesFront.programsExecutive)
-			return
-		}
+		// if (value === FilterTypeProgramEnum.executive) {
+		// 	router.push(routesFront.programsExecutive)
+		// 	return
+		// }
 
 		dispatch({ type: ACTION.SET_TYPE, payload: value })
 		dispatch({
@@ -36,32 +37,42 @@ export const FilterCategories = ({ className }: FilterCategoriesProps) => {
 	return (
 		<ul className={cn(className, stls.list)}>
 			{LIST_FILTER_TYPE_PROGRAM?.map((category, idx) => (
-				<li className={stls.item} key={idx}>
-					<button
-						className={cn(stls.item__link, {
-							[stls.active]: category.value === state.programsConfig.type
-						})}
-						onClick={() => handlerOnClick(category.value)}
-					>
-						<span className={cn(stls.item__name)}>
-							{category.text}{' '}
-							{category.value === FilterTypeProgramEnum.executive && (
-								<span className={stls.premium}>Premium</span>
-							)}
-						</span>
-					</button>
-					{category.tooltip && (
-						<InfoTooltip
-							className={stls.info}
-							color='#18191A'
-							textColor='#fff'
-							programsPageActive={category.value === state.programsConfig.type}
+				<>
+					<li className={stls.item} key={idx}>
+						<button
+							className={cn(stls.item__link, {
+								[stls.active]: category.value === state.programsConfig.type
+							})}
+							onClick={() => handlerOnClick(category.value)}
 						>
-							<p className={stls.infoDescription}>{category.tooltip}</p>
-						</InfoTooltip>
-					)}
-				</li>
+							<span className={cn(stls.item__name)}>{category.text}</span>
+						</button>
+						{category.tooltip && (
+							<InfoTooltip
+								className={stls.info}
+								color='#18191A'
+								textColor='#fff'
+								programsPageActive={
+									category.value === state.programsConfig.type
+								}
+							>
+								<p className={stls.infoDescription}>{category.tooltip}</p>
+							</InfoTooltip>
+						)}
+					</li>
+				</>
 			))}
+			<li>
+				<Link
+					href={`${routesFront.root}${routesFront.programsExecutive}`}
+					className={cn(stls.item__link)}
+				>
+					<span className={cn(stls.item__name)}>
+						{'Executive MBA '}
+						<span className={stls.premium}>Premium</span>
+					</span>
+				</Link>
+			</li>
 		</ul>
 	)
 }
